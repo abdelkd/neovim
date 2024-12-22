d4abaaaa28ffce45d1da3b7ab69e5c7d6a38dd61
abdelkd <abdelkarimdjelalda@gmail.com>
2024-12-23 00:19:11 +0100

    improve completion and nvim-tree

--- nvim/lua/kareem/plugins/completion.lua
+++ nvim/lua/kareem/plugins/completion.lua
@@ -4,10 +4,44 @@ end
 vim.g.did_load_completion_plugin = true
 
 vim.api.nvim_create_autocmd('InsertEnter', {
+  pattern = '*',
   callback = function()
     local cmp = require('cmp')
     local luasnip = require('luasnip')
     local lspkind = require('lspkind')
+
+    lspkind.init {
+      -- Enable text-based completion icons
+      with_text = true,
+      -- Define symbols (e.g., for function, variable, etc.)
+      symbol_map = {
+        Text = '', -- Text symbol
+        Method = '', -- Method symbol
+        Function = '', -- Function symbol
+        Constructor = '', -- Constructor symbol
+        Field = '', -- Field symbol
+        Variable = '', -- Variable symbol
+        Class = 'ﴯ', -- Class symbol
+        Interface = '了', -- Interface symbol
+        Module = '', -- Module symbol
+        Property = '', -- Property symbol
+        Unit = '', -- Unit symbol
+        Value = '', -- Value symbol
+        Enum = '', -- Enum symbol
+        Keyword = '', -- Keyword symbol
+        Snippet = '', -- Snippet symbol
+        Color = '', -- Color symbol
+        File = '', -- File symbol
+        Reference = '', -- Reference symbol
+        Folder = '', -- Folder symbol
+        EnumMember = '', -- Enum member symbol
+        Constant = '', -- Constant symbol
+        Struct = '', -- Struct symbol
+        Event = '', -- Event symbol
+        Operator = '', -- Operator symbol
+        TypeParameter = '', -- Type parameter symbol
+      },
+    }
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require('luasnip.loaders.from_vscode').lazy_load()
@@ -24,25 +58,32 @@ vim.api.nvim_create_autocmd('InsertEnter', {
       mapping = cmp.mapping.preset.insert {
         ['<C-k>'] = cmp.mapping.select_prev_item(), -- previous suggestion
         ['<C-j>'] = cmp.mapping.select_next_item(), -- next suggestion
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(), -- show completion suggestions
-        ['<C-e>'] = cmp.mapping.abort(),        -- close completion window
+        ['<C-e>'] = cmp.mapping.abort(), -- close completion window
         ['<CR>'] = cmp.mapping.confirm { select = false },
       },
       -- sources for autocompletion
       sources = cmp.config.sources {
         { name = 'nvim_lsp' }, -- LSP
         { name = 'luasnip' }, -- snippets
-        { name = 'buffer' },  -- text within current buffer
-        { name = 'path' },    -- file system paths
+        { name = 'buffer' }, -- text within current buffer
+        { name = 'path' }, -- file system paths
       },
 
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format {
+          mode = 'symbol_text',
           maxwidth = 50,
           ellipsis_char = '...',
+          menu = {
+            buffer = '[Buffer]',
+            nvim_lsp = '[LSP]',
+            luasnip = '[Snippet]',
+            path = '[Path]',
+          },
         },
       },
     }
--- nvim/lua/kareem/plugins/nvim-tree.lua
+++ nvim/lua/kareem/plugins/nvim-tree.lua
@@ -3,27 +3,29 @@ if vim.g.did_load_nvim_tree_plugin then
 end
 vim.g.did_load_nvim_tree_plugin = true
 
-local nvimtree = require("nvim-tree")
+local nvimtree = require('nvim-tree')
 
 -- recommended settings from nvim-tree documentation
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-nvimtree.setup({
+nvimtree.setup {
   view = {
+    size = 'right',
     width = 35,
     relativenumber = true,
   },
   -- change folder arrow icons
   renderer = {
+    highlight_git = true,
     indent_markers = {
       enable = true,
     },
     icons = {
       glyphs = {
         folder = {
-          arrow_closed = "", -- arrow when folder is closed
-          arrow_open = "", -- arrow when folder is open
+          arrow_closed = '', -- arrow when folder is closed
+          arrow_open = '', -- arrow when folder is open
         },
       },
     },
@@ -33,23 +35,39 @@ nvimtree.setup({
   -- window splits
   actions = {
     open_file = {
+      resize_window = true,
       window_picker = {
         enable = false,
       },
     },
   },
   filters = {
-    custom = { ".DS_Store" },
+    custom = { '.DS_Store' },
   },
   git = {
-    ignore = false,
+    enable = true,
+    unstaged = '✗',
+    staged = '✓',
+    untracked = '?',
+    renamed = '➜',
+    deleted = '',
+    ignored = '◌',
   },
-})
+  diagnostics = {
+    enable = true,
+    icons = {
+      error = '', -- Error icon
+      warning = '', -- Warning icon
+      hint = '', -- Hint icon
+      info = '', -- Info icon
+    },
+  },
+}
 
 -- set keymaps
 local keymap = vim.keymap -- for conciseness
 
-keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+keymap.set('n', '<leader>ee', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' }) -- toggle file explorer
+keymap.set('n', '<leader>ef', '<cmd>NvimTreeFindFileToggle<CR>', { desc = 'Toggle file explorer on current file' }) -- toggle file explorer on current file
+keymap.set('n', '<leader>ec', '<cmd>NvimTreeCollapse<CR>', { desc = 'Collapse file explorer' }) -- collapse file explorer
+keymap.set('n', '<leader>er', '<cmd>NvimTreeRefresh<CR>', { desc = 'Refresh file explorer' }) -- refresh file explorer
